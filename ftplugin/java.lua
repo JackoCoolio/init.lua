8310a81738523befa354bd73cfbdd1170bfd0073
Jackson Wambolt <jtwambolt@gmail.com>
2023-02-03 00:55:52 -0600

    Don't start jdtls if not installed

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,12 +1,18 @@
 -- assumes that jdtls has been install with :Mason
 local jdtls_bin = vim.fn.stdpath("data") .. "/mason/bin/jdtls"
 
+-- only start if jdtls is installed
+if vim.fn.filereadable(jdtls_bin) == 0 then
+    print("jdtls: not installed")
+    return
+end
+
 local config = {
     cmd = {
         jdtls_bin,
     },
     root_dir = vim.fs.dirname(vim.fs.find({ '.gradlew', '.git', 'mvnw' }, { upward = true })[1]),
-    on_attach = function(client, bufnr)
+    on_attach = function(_, bufnr)
         local bufopts = { noremap = true, silent = true, buffer = bufnr }
 
         DoLSPMappings(bufopts)
