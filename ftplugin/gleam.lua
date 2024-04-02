b11df88c7208756288ff2f6290fcf30a48fe243c
Jackson Wambolt <jtwambolt@gmail.com>
2024-04-01 19:02:41 -0500

    ft(gleam): set tab width and format on save

---
+++ ftplugin/gleam.lua
@@ -0,0 +1,13 @@
+local utils = require("jackocoolio.util")
+
+-- gleam convention is 2 space tabs
+utils.set_tab_width(2)
+
+vim.api.nvim_create_augroup("AutoFormat", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.gleam",
+    group = "AutoFormat",
+    callback = function()
+        vim.lsp.buf.format()
+    end
+})
