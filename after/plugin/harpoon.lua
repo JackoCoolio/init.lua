33cdd62fde77d1899adf746a0b43f7ca81aeaf0d
Jackson Wambolt <jtwambolt@gmail.com>
2024-04-13 23:32:00 -0500

    harpoon: fix append deprecation warning

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,7 +8,7 @@ local opts = {
     silent = true,
 }
 
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>m", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 vim.keymap.set("n", "<leader>h", function() harpoon:list():select(1) end, opts)
 vim.keymap.set("n", "<leader>j", function() harpoon:list():select(2) end, opts)
