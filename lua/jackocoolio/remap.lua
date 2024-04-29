97217df06c43adc7f4b06f46d8123239f740a5b5
Jackson Wambolt <jtwambolt@gmail.com>
2024-04-28 19:44:40 -0500

    Remove `<leader>s` remap

--- lua/jackocoolio/remap.lua
+++ lua/jackocoolio/remap.lua
@@ -38,7 +38,7 @@ vim.keymap.set("n", "<leader>fd", function()
 end)
 
 -- replace all occurrences of current word
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- make current file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
