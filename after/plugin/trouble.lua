9f444e92d0cc0e4f98cb1629933935af2f4182bc
Jackson Wambolt <jtwambolt@gmail.com>
2024-04-28 20:33:29 -0500

    Don't auto open/close trouble

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -2,7 +2,11 @@ local trouble = require("trouble")
 
 trouble.setup {
     multiline = true,
-    auto_open = true,
-    auto_close = true,
+    auto_open = false,
+    auto_close = false,
     mode = "workspace_diagnostics",
 }
+
+vim.keymap.set("n", "<leader>od", function()
+    trouble.toggle()
+end)
