14d7c298f7156d1914932a39bdcf6223cd2f87df
Jackson Wambolt <jtwambolt@gmail.com>
2024-12-29 09:00:31 +0000

    add go and sql formatters

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -8,6 +8,8 @@ return {
 			lua = { "stylua" },
 			rust = { "rustfmt", lsp_format = "fallback" },
 			javascript = { "prettier", stop_after_first = true },
+			go = { "gofmt", append_args = { "-s" } },
+			sql = { "sqlfluff" },
 		},
 		format_on_save = {
 			timeout_ms = 500,
