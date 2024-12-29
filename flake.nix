dfcc2ce34bb209bbdc09f04773d347081026d67e
Jackson Wambolt <jtwambolt@gmail.com>
2024-12-29 08:54:27 +0000

    format nix files

--- flake.nix
+++ flake.nix
@@ -6,17 +6,23 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in {
+      in
+
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             lua-language-server
             stylua
           ];
         };
+
+        formatter = pkgs.nixfmt-rfc-style;
       }
     );
 }
