7713e094b132e7c0d5cc8c3901723b333872ed86
Jackson Wambolt <jtwambolt@gmail.com>
2025-02-03 01:58:59 +0000

    Replace cmp with blink

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,61 +1,3 @@
--- return {
--- 	"hrsh7th/nvim-cmp",
---
--- 	dependencies = {
--- 		"L3MON4D3/LuaSnip",
--- 		"hrsh7th/cmp-nvim-lsp",
--- 		"hrsh7th/cmp-nvim-lua",
--- 		"hrsh7th/cmp-nvim-lua",
--- 		"hrsh7th/cmp-nvim-lsp-signature-help",
--- 		"hrsh7th/cmp-buffer",
--- 		"hrsh7th/cmp-path",
--- 		"hrsh7th/cmp-cmdline",
--- 	},
---
--- 	config = function()
--- 		local cmp = require("cmp")
--- 		cmp.setup({
--- 			snippet = {
--- 				expand = function(args)
--- 					require("luasnip").lsp_expand(args.body)
--- 				end,
--- 			},
--- 			window = {
--- 				completion = cmp.config.window.bordered(),
--- 				documentation = cmp.config.window.bordered(),
--- 			},
--- 			mapping = cmp.mapping.preset.insert({
--- 				["<c-space>"] = cmp.mapping.complete(),
--- 				["<c-e>"] = cmp.mapping.abort(),
--- 				["<cr>"] = cmp.mapping.confirm({ select = false }),
--- 				["<tab>"] = cmp.mapping(function(fallback)
--- 					if cmp.visible() then
--- 						cmp.select_next_item()
--- 					else
--- 						fallback()
--- 					end
--- 				end, { "i", "s" }),
--- 				["<s-tab>"] = cmp.mapping(function(fallback)
--- 					if cmp.visible() then
--- 						cmp.select_prev_item()
--- 					else
--- 						fallback()
--- 					end
--- 				end),
--- 			}),
--- 			sources = cmp.config.sources({
--- 				{ name = "nvim_lsp" },
--- 				{ name = "nvim_lsp_signature_help" },
--- 				{ name = "nvim_lua" },
--- 				-- { name = "luasnip" },
--- 			}, {
--- 				{ name = "buffer", keyword_length = 3 },
--- 				{ name = "path" },
--- 			}),
--- 		})
--- 	end,
--- }
-
 return {
 	"saghen/blink.cmp",
 
@@ -64,7 +6,19 @@ return {
 	build = "nix run .#build-plugin",
 
 	opts = {
-		keymap = { preset = "default" },
+		keymap = {
+			-- disable 'default' preset
+			preset = "none",
+
+			["<CR>"] = { "accept", "fallback" },
+			["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<Down>"] = { "select_next", "snippet_forward", "fallback" },
+
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+			["<Up>"] = { "select_prev", "snippet_backward", "fallback" },
+		},
 
 		appearance = {
 			use_nvim_cmp_as_default = true,
@@ -77,6 +31,27 @@ return {
 				"path",
 				"snippets",
 				"buffer",
+			},
+		},
+
+		completion = {
+			list = {
+				selection = {
+					preselect = false,
+					auto_insert = false,
+				},
+			},
+			ghost_text = {
+				enabled = false,
+			},
+			menu = {
+				draw = {
+					treesitter = { "lsp" },
+				},
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 100,
 			},
 		},
 	},
